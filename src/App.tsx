b5747c7e2ddb3dbd3ff024481d770c0bd11afcc4
Anup50 <kca3734@gmail.com>
2025-01-22 22:47:28 +0545

    routes, images

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./core/public/Home";
-import SignUpPage from "./core/public/Signin";
+import SignInPage from "./core/public/Signin";
+import SignUpPage from "./core/public/Signup";
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignUpPage />} />
+        <Route path="/signup" element={<SignInPage />} />
       </Routes>
     </Router>
   );
