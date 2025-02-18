ba332b411f42acaa131247a393879adbdf4a63b1
Anup50 <kca3734@gmail.com>
2025-02-18 19:15:51 +0545

    api endpints for getjobseeker id and get recommended

--- src/Api.ts
+++ src/Api.ts
@@ -7,6 +7,11 @@ const Api = axios.create({
     "Content-Type": "application/json",
   },
 });
-export const registerUser = (data: { name: string; email: string; password: string; role: string | null; }) => Api.post("api/user", data);
+
+export const registerUser = (data: { name: string; email: string; password: string; role: string | null; }) => Api.post("api/user/register", data);
 export const loginUser = (data: { email: string; password: string; }) => Api.post("api/user/login", data);
-export const getAllJobs = () => Api.get("/api/jobs");
+export const getAllJobs = () => Api.get("/api/job");
+export const getJobSeekerId = () => Api.get("/api/jobseeker/getJobSeekerId");
+export const getRecommendedJobs = (jobSeekerId: string) => {
+  return Api.get(`/api/job/recommended/${jobSeekerId}`);
+};
