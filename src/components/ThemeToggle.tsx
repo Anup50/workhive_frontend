71e2d60deaaf86f6b24e6a2c1043edbc2b86a551
Anup50 <kca3734@gmail.com>
2025-02-24 15:44:52 +0545

    component for changing themes

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,21 @@
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "../context/ThemeContext";
+
+const ThemeToggle = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="btn btn-ghost btn-circle"
+      aria-label={`Toggle ${theme === "light" ? "dark" : "light"} mode`}
+    >
+      {theme === "light" ? (
+        <MoonIcon className="w-5 h-5" />
+      ) : (
+        <SunIcon className="w-5 h-5" />
+      )}
+    </button>
+  );
+};
+export default ThemeToggle;
