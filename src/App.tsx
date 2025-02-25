143a3ae281c8c5564188e6901635c375125ee412
Anup50 <kca3734@gmail.com>
2025-02-25 11:14:12 +0545

    navbar renders above certain pages in public routes only now

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { Suspense, lazy } from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Outlet,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import { Toaster } from "sonner";
+import Navbar from "./components/Navbar";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./context/ProtectedRoute";
 
+const PublicLayout = () => {
+  const location = useLocation();
+  const showNavbar =
+    location.pathname !== "/signin" && location.pathname !== "/signup";
+
+  return (
+    <>
+      {showNavbar && <Navbar />}
+      <Outlet />
+    </>
+  );
+};
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -19,7 +38,7 @@ const SignInPage = lazy(() => import("./core/public/Signin"));
 const SignUpPage = lazy(() => import("./core/public/Signup"));
 const User = lazy(() => import("./core/private/JobSeeker/Home"));
 const Employer = lazy(() => import("./core/private/employer/Index"));
-
+const Search = lazy(() => import("./shared/Search/SearchPage"));
 const App: React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
@@ -29,7 +48,11 @@ const App: React.FC = () => {
           <Suspense fallback={<div>Loading...</div>}>
             <Routes>
               {/* Public Routes */}
-              <Route path="/" element={<Home />} />
+              <Route element={<PublicLayout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/search/:query" element={<Search />} />
+              </Route>
+
               <Route path="/signin" element={<SignInPage />} />
               <Route path="/signup" element={<SignUpPage />} />
 
