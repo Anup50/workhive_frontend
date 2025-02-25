6c0635d8effab18ffb3ed0d70bc085f30e279977
Anup50 <kca3734@gmail.com>
2025-02-25 11:05:57 +0545

    animation wrpper component

---
+++ src/components/AnimationWrapper.tsx
@@ -0,0 +1,59 @@
+// import { motion } from "framer-motion";
+
+// const AnimationWrapper = ({
+//   children,
+//   keyValue,
+//   initial = { opacity: 0 },
+//   animate = { opacity: 1 },
+//   transition = { duration: 2 },
+//   className,
+// }) => {
+//   return (
+//     <motion.div
+//       key={keyValue}
+//       initial={initial}
+//       animate={animate}
+//       transition={transition}
+//       className={className}
+//     >
+//       {children}
+//     </motion.div>
+//   );
+// };
+
+// export default AnimationWrapper;
+import { AnimatePresence, motion, TargetAndTransition } from "framer-motion";
+
+interface AnimationWrapperProps {
+  children: React.ReactNode;
+  keyValue?: string | number;
+  initial?: TargetAndTransition;
+  animate?: TargetAndTransition;
+  transition?: { duration?: number; delay?: number };
+  className?: string;
+}
+
+const AnimationWrapper: React.FC<AnimationWrapperProps> = ({
+  children,
+  keyValue,
+  initial = { opacity: 0 },
+  animate = { opacity: 1 },
+  transition = { duration: 2 },
+  className,
+}) => {
+  return (
+    <AnimatePresence>
+      <motion.div
+        key={keyValue}
+        initial={initial}
+        animate={animate}
+        transition={transition}
+        className={className}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  );
+};
+
+export default AnimationWrapper;
