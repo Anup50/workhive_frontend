5dd8706d13a991d0ebd185a65eeb68010a044357
Anup50 <kca3734@gmail.com>
2025-02-25 12:00:56 +0545

    404 page

---
+++ src/shared/PageNotFound.tsx
@@ -0,0 +1,9 @@
+const PageNotFound = () => {
+  return (
+    <section className="h-cover relative p-10 flex flex-col items-center gap-20 text-center">
+      <img src="" />
+    </section>
+  );
+};
+
+export default PageNotFound;
